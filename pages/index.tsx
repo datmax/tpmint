cf5a0a0bbd6c0ba04a1b60a096553c27a07bbaad
datmax <maxdatsun10@gmail.com>
2023-05-02 00:05:16 +0200

    removed logs

--- pages/index.tsx
+++ pages/index.tsx
@@ -108,7 +108,6 @@ export default function Home() {
     }
   }, [isPausedLoading, isWhitelistedLoading, wlOnlyLoading, supplyLoading]);
 
-  console.log(isWhitelisted, wlOnly, address);
   //set body opacity to 1 after 0.2s
   useEffect(() => {
     setTimeout(() => {
